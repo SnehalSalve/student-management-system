59fe9cb091dfb663ddde2a6aae840e1384debba0
Snehal Salve <69302530+SnehalSalve@users.noreply.github.com>
2024-07-24 19:29:42 +0530

    create student management entity

---
+++ Student.java
@@ -0,0 +1,64 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int roll_no;
+	private String first_name;
+	private String last_name;
+	private float percentage;
+	private String city;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int roll_no, String first_name, String last_name, float percentage, String city) {
+		super();
+		this.roll_no = roll_no;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.percentage = percentage;
+		this.city = city;
+	}
+	public int getRoll_no() {
+		return roll_no;
+	}
+	public void setRoll_no(int roll_no) {
+		this.roll_no = roll_no;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public float getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(float percentage) {
+		this.percentage = percentage;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
+}
